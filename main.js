c5a2234235b8d9c9e46aa702581a0be88326369e
GianBo <gianbo251@gmail.com>
2025-03-23 21:28:05 +0100

    Comprobación de respuesta API

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("HOLA MUNDO")
+
+
+// Solicitud de datos de la API  con fetch (prueba) 
+
+fetch("https://api.rawg.io/api/games?key=6a93726f4bd64b22bf9cf5bb08cb0f7f")
+    .then(response => response.json()) // Convertir la respuesta a JSON transformando la respuesta en un objeto luego de que llegue la respuesta 
+    .then(data => console.log(data.results)) // Imprimir los datos en consola para verificar que funcione correctamente ( status:ok) 
